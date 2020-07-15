02758562d776fd142ab7accb18a20cec80372cca
Ameer Hamza <ameernormie@gmail.com>
2020-07-15 12:56:45 +0500

    refactor: pass in reference to the nums vector

--- rust/easy/two_sum/src/main.rs
+++ rust/easy/two_sum/src/main.rs
@@ -5,20 +5,20 @@ fn main() {
     let nums = vec![2, 7, 11, 13];
     let target = 9;
 
-    let brute_force_result = brute_force_two_sum(nums, target);
+    let brute_force_result = brute_force_two_sum(&nums, target);
     println!("The brute_force_result is {:?}", brute_force_result);
 
-    let hash_map_result = hash_map_two_sum(vec![2, 7, 11, 13], target);
+    let hash_map_result = hash_map_two_sum(&nums, target);
     println!("The hash_map_result is {:?}", hash_map_result);
 
-    let optimized_hash_map_result = optimized_hash_map_two_sum(vec![2, 7, 11, 13], target);
+    let optimized_hash_map_result = optimized_hash_map_two_sum(&nums, target);
     println!(
         "The optimized_hash_map_result is {:?}",
         optimized_hash_map_result
     );
 }
 
-fn brute_force_two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+fn brute_force_two_sum(nums: &Vec<i32>, target: i32) -> Vec<i32> {
     for (i, &a) in nums.iter().enumerate() {
         for (j, &b) in nums.iter().enumerate() {
             if i != j && a + b == target {
@@ -29,7 +29,7 @@ fn brute_force_two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     return vec![];
 }
 
-fn hash_map_two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+fn hash_map_two_sum(nums: &Vec<i32>, target: i32) -> Vec<i32> {
     let mut hm = HashMap::new();
     for (i, &val) in nums.iter().enumerate() {
         hm.insert(val, i as i32);
@@ -48,7 +48,7 @@ fn hash_map_two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     vec![]
 }
 
-fn optimized_hash_map_two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+fn optimized_hash_map_two_sum(nums: &Vec<i32>, target: i32) -> Vec<i32> {
     let mut hm = HashMap::new();
 
     for (i, &val) in nums.iter().enumerate() {
